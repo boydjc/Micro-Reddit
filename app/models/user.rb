9467619ff1ba2e90302cc832fda0971a424cdd28
Joshua Boyd <jaboydfamily@gmail.com>
2022-03-24 22:08:07 -0400

    added Post db model and field validations - added one to many association between User and Post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-	validates :username, presence: true
-	validates :username, length: { in: 2..10 }
-	validates :username, uniqueness: true
+  validates :username, presence: true
+  validates :username, length: { in: 2..10 }
+  validates :username, uniqueness: true
 	
-	validates :email, presence: true
-	validates :email, uniqueness: true
-	validates :email, format: { with: /\A[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\z/, 
-	  message: "must be an email" }
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\z/, 
+            message: "must be an email" }
+	
+  has_many :posts
 end
